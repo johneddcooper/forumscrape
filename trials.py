e6ae97f946ac8f15bda341ade974ad0a7fe8d424
John Cooper <john.edd.cooper@gmail.com>
2018-12-20 20:37:28 -0500

    added test jupeter notebook

---
+++ trials.py
@@ -0,0 +1,7 @@
+
+#view-source:https://incels.is/threads/why-we-are-superior-to-nice-guys-and-neck-beards.95871/#post-1863255
+import requests
+from bs4 import BeautifulSoup
+url = "https://incels.is/threads/why-we-are-superior-to-nice-guys-and-neck-beards.95871/#post-1863255"
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
