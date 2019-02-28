629bf0162f81ad1efb037735c72cfa844b4341c1
John Cooper <john.edd.cooper@gmail.com>
2019-02-27 21:55:25 -0500

    Added mongodb pipeline

--- forum_spider/forum_spider/settings.py
+++ forum_spider/forum_spider/settings.py
@@ -64,9 +64,13 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'forum_spider.pipelines.ForumSpiderPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'forum_spider.pipelines.ForumSpiderPipeline': 300,
+}
+
+MONGODB_URI = "localhost"
+MONGODB_DB = "scrapy_test"
+
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
